aefa43fc47e7aed9fd07c5dca574f11f14a7f1c7
aruyume <aruukerahmanovaa@gmail.com>
2024-10-19 16:29:52 +0600

    hw7

--- app/src/main/java/com/example/aruuke_rakhmanova_hw_7/Activity2.java
+++ app/src/main/java/com/example/aruuke_rakhmanova_hw_7/Activity2.java
@@ -1,6 +1,10 @@
 package com.example.aruuke_rakhmanova_hw_7;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,22 @@ public class Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        int data = getIntent().getIntExtra("result", 0) ;
+        TextView textView = findViewById(R.id.text_ex);
+        textView.setText(String.valueOf(data));
+
+        Spinner spinner = findViewById(R.id.dropdown_menu);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.choice_color, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Button buttonHeart = findViewById(R.id.heart_button);
+        buttonHeart.setOnClickListener(v -> {
+            if (buttonHeart.getBackground().getConstantState().equals(getResources().getDrawable(R.drawable.ic_heart).getConstantState())) {
+                buttonHeart.setBackgroundResource(R.drawable.ic_heart_2);
+            } else {
+                buttonHeart.setBackgroundResource(R.drawable.ic_heart);
+            }
         });
     }
 }
--- app/src/main/java/com/example/aruuke_rakhmanova_hw_7/MainActivity.java
+++ app/src/main/java/com/example/aruuke_rakhmanova_hw_7/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.aruuke_rakhmanova_hw_7;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,13 +15,14 @@ public class MainActivity extends AppCompatActivity {
     Integer firstNumber, secondNumber;
     boolean isOperation;
     String currentOperation;
-    String currentText;
+    Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         result = findViewById(R.id.result);
+        btnNext = findViewById(R.id.btnNext);
     }
 
     public void numberClick(View view) {
@@ -34,36 +39,49 @@ public class MainActivity extends AppCompatActivity {
     public void operationClick(View view) {
         if (view.getId() == R.id.clearBtn) {
             result.setText("");
-        } else if (view.getId() == R.id.plusBtn) {
-            firstNumber = Integer.valueOf(result.getText().toString());
-            currentOperation = "+";
-        } else if (view.getId() == R.id.minusBtn) {
-            firstNumber = Integer.valueOf(result.getText().toString());
-            currentOperation = "-";
-        } else if (view.getId() == R.id.multiplyBtn) {
-            firstNumber = Integer.valueOf(result.getText().toString());
-            currentOperation = "*";
-        } else if (view.getId() == R.id.divideBtn) {
-            firstNumber = Integer.valueOf(result.getText().toString());
-            currentOperation = "/";
-
-        } else if (view.getId() == R.id.equalBtn) {
-            secondNumber = Integer.valueOf(result.getText().toString());
-
-            if (currentOperation == ("+")) {
-                Integer resultPlus = firstNumber + secondNumber;
-                result.setText(String.valueOf(resultPlus));
-            } else if (currentOperation == ("-")) {
-                Integer resultMinus = firstNumber - secondNumber;
-                result.setText(String.valueOf(resultMinus));
-            } else if (currentOperation == ("*")) {
-                Integer resultMultiply = firstNumber * secondNumber;
-                result.setText(String.valueOf(resultMultiply));
-            } else if (currentOperation == ("/")) {
-                Integer resultDivine = firstNumber / secondNumber;
-                result.setText(String.valueOf(resultDivine));
+        } else {
+            if (view.getId() == R.id.plusBtn) {
+                firstNumber = Integer.valueOf(result.getText().toString());
+                currentOperation = "+";
+            } else if (view.getId() == R.id.minusBtn) {
+                firstNumber = Integer.valueOf(result.getText().toString());
+                currentOperation = "-";
+            } else if (view.getId() == R.id.multiplyBtn) {
+                firstNumber = Integer.valueOf(result.getText().toString());
+                currentOperation = "*";
+            } else if (view.getId() == R.id.divideBtn) {
+                firstNumber = Integer.valueOf(result.getText().toString());
+                currentOperation = "/";
             }
+            else if (view.getId() == R.id.equalBtn) {
+                secondNumber = Integer.valueOf(result.getText().toString());
+                btnNext.setVisibility(View.VISIBLE);
+                result.setText(String.valueOf(result(firstNumber, secondNumber, currentOperation)));
+                int sum = result(firstNumber, secondNumber, currentOperation);
+                btnNext.setOnClickListener(v -> {
+                    Intent intent = new Intent(MainActivity.this, Activity2.class);
+                    intent.putExtra("result", sum);
+                    startActivity(intent);
+                });
         }
-        isOperation = true;
+            isOperation = true;
+
+        }
+    }
+
+
+    private int result(int firstNumber, int secondNumber, String currentOperation) {
+        switch (currentOperation) {
+            case "+":
+                return firstNumber + secondNumber;
+            case "-":
+                return firstNumber - secondNumber;
+            case "*":
+                return firstNumber * secondNumber;
+            case "/":
+                return firstNumber / secondNumber;
+            default:
+                return 0;
+        }
     }
 }
